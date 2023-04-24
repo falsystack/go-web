9f36de2430b1ebe6c74f9db7f6c50c5df40ab434
falsystack <falsystack@gmail.com>
2023-04-25 08:24:52 +0900

    feat(015): tcp server

---
+++ 015-understanding-TCP-servers/01_write/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	// TCP Server
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err)
+			continue
+		}
+
+		io.WriteString(conn, "Hello from TCP server")
+		fmt.Fprintln(conn, "How is your day?")
+		fmt.Fprintf(conn, "Well, I hope!")
+	}
+}
