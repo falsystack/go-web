29d7698a37bfbe6417258b4d3cea05970aa0f13f
falsystack <falsystack@gmail.com>
2023-04-26 21:02:11 +0900

    feat(015-02): read & write

---
+++ 015-understanding-TCP-servers/03_read_write/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if hasErr(err) {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if hasErr(err) {
+			log.Fatalln(err)
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	// deadlineを設定することもできる
+	// err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "貴方の声が聞こえます：%s\n", ln)
+	}
+	defer conn.Close()
+
+	fmt.Println("ここまでたどり着けない")
+}
+
+func hasErr(err error) bool {
+	return err != nil
+}
