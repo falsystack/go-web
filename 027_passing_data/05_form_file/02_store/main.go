badd65cdd376482e501da81b48453b5b4872d838
falsystack <falsystack@gmail.com>
2023-06-14 13:33:03 +0900

    feat: ignore unuse var

--- 027_passing_data/05_form_file/02_store/main.go
+++ 027_passing_data/05_form_file/02_store/main.go
@@ -54,7 +54,7 @@ func foo(w http.ResponseWriter, req *http.Request) {
 		}
 		defer dst.Close()
 
-		n, err := dst.Write(bs)
+		_, err = dst.Write(bs)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
