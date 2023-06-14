989c5ea8c17bd6645d7bf61c5ed72b22d3950935
falsystack <falsystack@gmail.com>
2023-06-14 16:05:41 +0900

    feat: 303 SeeOther

---
+++ 028_redirect/01_303_SeeOther/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/barred", barred)
+
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
+	log.Println("Server starts...")
+	log.Fatalln(http.ListenAndServe(":8080", nil))
+
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	log.Println("[foo] Request method :", req.Method)
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	log.Println("[bar] Request method :", req.Method)
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+func barred(w http.ResponseWriter, req *http.Request) {
+	log.Println("[barred] Request method :", req.Method)
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
